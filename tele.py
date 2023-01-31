7a2699da0e1c150c55144152302065a74fa7e308
beomobile <60918933+beomobile@users.noreply.github.com>
2023-01-31 17:24:38 +0700

    Update tele.py

--- tele.py
+++ tele.py
@@ -1,9 +1,9 @@
 from telegram import *
 from telegram.ext import *
 
-bot = Bot("Paste your Telegram bot token here")
+bot = Bot("AAGXBfTW8qFp4OUz4YC4IdojVyWEkUhuFtA")
 #print(bot.get_me())
-updater = Updater("Paste your Telegram bot token here", use_context=True)
+updater = Updater("AAGXBfTW8qFp4OUz4YC4IdojVyWEkUhuFtA", use_context=True)
 
 dispatcher= updater.dispatcher
 
@@ -11,7 +11,7 @@ dispatcher= updater.dispatcher
 def test_func(update:Update, context:CallbackContext):
     bot.send_message(
         chat_id = update.effective_chat.id,
-        text ="tutorial link : https://youtu.be/Tm5u97I7OrM",
+        text ="tutorial link : https://m.me/112053085065108?ref=hotro",
     )
 start_value = CommandHandler('motion_detection',test_func)
 
